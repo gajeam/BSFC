97ef0f3a6625903bc83cbb9b12a425b31713365b
Arjun Mehta <arjunmehta94@berkeley.edu>
2016-04-20 19:49:14 -0700

    renames basic -> inventory, cleans up comments

--- inventory.py
+++ inventory.py
@@ -10,30 +10,26 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('BSFC-e1ba6e23904
 gc = gspread.authorize(credentials)
 
 wks = gc.open_by_url("https://docs.google.com/spreadsheets/d/12sv4lmEC3OhesEd0yPHy1c_Yu3wtOEBVLxie3TxYIMo/edit#gid=0").sheet1
-#wks = gc.open("YOLO").sheet1
 import requests
 req = requests.get(url, headers=headers)
-#response = urllib2.urlopen(req)
-#html = response.read()
 data = req.json()['elements']
-print(data)
 nameCell = wks.find('NAME')
+priceCell = wks.find('PRICE')
 costCell = wks.find('COST')
 stockcountCell = wks.find('STOCKCOUNT')
-#print(nameCell.row, nameCell.col)
 count = int(nameCell.row) + 1
-# for item in data:
-# 	if 'stockCount' not in item:
-# 		item['stockCount'] = 'NULL'
-# 	if 'cost' not in item:
-# 		item['cost'] = 'NULL'
-# 	#print(item['name'], item['cost'], item['stockCount'])
-# 	wks.update_cell(count, nameCell.col, item['name'])
-# 	wks.update_cell(count, costCell.col, item['cost'])
-# 	wks.update_cell(count, stockcountCell.col, item['stockCount'])
-# 	count += 1
-# print(wks.acell('A1').value)
-# print(wks.acell('B1').value)
-# wks.update_acell('B2', 'HELLO')
-#json_obj = json.loads(html)
-#print json_obj
+for item in data:
+	if 'stockCount' not in item:
+		item['stockCount'] = 'NULL'
+	if 'cost' not in item:
+		item['cost'] = 'NULL'
+	if 'price' not in item:
+		item['price'] = 'NULL'
+	wks.update_cell(count, nameCell.col, item['name'])
+	wks.update_cell(count, priceCell.col, item['price'])
+	wks.update_cell(count, costCell.col, item['cost'])
+	wks.update_cell(count, stockcountCell.col, item['stockCount'])
+	count += 1
+### TODO
+
+# ORDERS
