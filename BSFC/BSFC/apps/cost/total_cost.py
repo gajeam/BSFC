466cc9b328b85cfb03dc94117ed9f48646f789ed
Katie <katherinelatimer2013@u.northwestern.edu>
2016-08-06 18:44:39 -0700

    other mistakes fixed

--- BSFC/BSFC/apps/cost/total_cost.py
+++ BSFC/BSFC/apps/cost/total_cost.py
@@ -2,11 +2,11 @@ from BSFC.apps.item.models import Item
 
 def total_cost()
 
-	A = Items.objects.all()
+	A = Item.objects.all()
 
 	total = 0
 
-	for thing in A
+	for thing in A:
 		total += thing.cost.item_cost*thing.count
 
 	return total
