379a9ffc9e8264dd6f6f9c76eb9e686b08595ecc
Arjun Mehta <arjun@sighten.io>
2016-08-13 18:41:08 -0700

    adds django extensions

--- BSFC/BSFC/settings.py
+++ BSFC/BSFC/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'BSFC.apps.item',
     'BSFC.apps.cost',
     'BSFC.apps.revenue'
