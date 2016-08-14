d14d9170920a91a2a13e0b38f7a4c11cf04b56c2
Arjun Mehta <arjun@sighten.io>
2016-08-13 18:39:47 -0700

    constants for revenue

---
+++ BSFC/BSFC/apps/revenue/constants.py
@@ -0,0 +1,16 @@
+# price type choices
+FIXED = 'FI'
+PER_UNIT = 'P/U'
+
+# category choices
+BAKERY = 'B'
+GROCERY = 'G'
+BULK = 'BU'
+BSFC = 'BSFC'
+PRODUCE = 'P'
+HEALTH_AND_BEAUTY = 'H&B'
+HOUSEHOLD = 'H'
+BEVERAGE = 'BEV'
+CHILL = 'C'
+GRAB_AND_GO = 'G&G'
+FROZEN = 'FR'
