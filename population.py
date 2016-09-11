e826d34f5ae8e786d43861bf122588fd13119d7d
Arjun Mehta <arjun@sighten.io>
2016-09-10 18:47:30 -0700

    implement api calls, cleanup

--- population.py
+++ population.py
@@ -1,19 +1,46 @@
 from BSFC.apps.item.models import Item
 from BSFC.apps.cost.models import Cost
 from BSFC.apps.revenue.models import Revenue
+from BSFC.public.api_data import get_api_data
+import time
+from datetime import datetime, timedelta
 
 def populate_single_day():
-	"""use payments from the day to record quantity of each item contributing to revenue calculation"""
-	todays_payments = #call to REST API to get list of all payments, expanding by 'order,' 'refunds,' and 'tender' and filtering by day
+    """use payments from the day to record quantity of each item contributing to revenue calculation"""
+    yesterday = datetime.today() + timedelta(days=-1)
+    yesterday_timestamp_ms = int(time.mktime(yesterday.timetuple())) * 1000
+    todays_payments = get_api_data(
+        'payments',
+        filterItems='createdTime>' + str(yesterday_timestamp_ms),
+        expandItems='order,refunds,tender'
+    )
+    #call to REST API to get list of all payments, expanding by 'order,' 'refunds,' and 'tender' and filtering by day
 
 	for payment_dict in todays_payments:
 		todays_date = #whatever convention we are using for dates...?
 		payment_type = payment_dict['tender']['label']
-		order_dict = #get corresponding order with payment['order']['id'], expanding by 'lineItems,' 'discounts'
+        order_id = payment_dict['order']['id']
+		order_dict = get_api_data(
+            'orders/' + str(order_id),
+            expandItems='lineItems,discounts'
+        )
+        #get corresponding order with payment['order']['id'], expanding by 'lineItems,' 'discounts'
 		for line_item_dict in order_dict['lineItems']['elements']
-			if Item.objects.filter(name = line_item_dict['name'], created_at.date() = todays_date).exists():
+			if Item.objects.filter(name=line_item_dict['name'], created_at.date()=todays_date).exists():
 				Item.objects.get(name = line_item_dict['name'], created_at.date() = todays_date).revenue.update_revenue_field(payment_type, item.quantity)
 			else:
-				item_dict = #call to REST API to get item; use line_item_dict['elements']['item']['id']
-				Items.objects.create(name = item_dict['name'], cost = Cost.objects.create(item_cost = item_dict['cost']), price = item_dict['price'], price_type = item_dict['priceType'], unit_name = item_dict['unitName'] if 'unitName' in item_dict else None, category = item_dict['categories']['elements'][0]['name'], revenue = Revenue.objects.create()) 
+                item_dict = get_api_data(
+                    'items/' + str(line_item_dict['item']['id']),
+                    expandItems='categories'
+                )
+                #call to REST API to get item; use line_item_dict['elements']['item']['id']
+                Items.objects.create(
+                    name=item_dict['name'],
+                    cost=Cost.objects.create(item_cost=item_dict['cost']),
+                    price=item_dict['price'],
+                    price_type=item_dict['priceType'],
+                    unit_name=item_dict['unitName'] if 'unitName' in item_dict else None,
+                    category=item_dict['categories']['elements'][0]['name'],
+                    revenue=Revenue.objects.create()
+                )
 
