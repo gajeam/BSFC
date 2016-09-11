198f0c10e8d857208b8b4d6978a30bd473a91339
Katie <katherinelatimer2013@u.northwestern.edu>
2016-09-10 18:10:32 -0700

    fixed small error in revenue update call in population

--- population.py
+++ population.py
@@ -11,9 +11,12 @@ def populate_single_day():
 		payment_type = payment_dict['tender']['label']
 		order_dict = #get corresponding order with payment['order']['id'], expanding by 'lineItems,' 'discounts'
 		for line_item_dict in order_dict['lineItems']['elements']
+
+			quantity = line_item_dict['unitQty']/1000 if 'unitQty' in line_item_dict else 1
+
 			if Item.objects.filter(name = line_item_dict['name'], created_at.date() = todays_date).exists():
-				Item.objects.get(name = line_item_dict['name'], created_at.date() = todays_date).revenue.update_revenue_field(payment_type, item.quantity)
+				Item.objects.get(name = line_item_dict['name'], created_at.date() = todays_date).revenue.update_revenue_field(payment_type, quantity)
 			else:
 				item_dict = #call to REST API to get item; use line_item_dict['elements']['item']['id']
-				Items.objects.create(name = item_dict['name'], cost = Cost.objects.create(item_cost = item_dict['cost']), price = item_dict['price'], price_type = item_dict['priceType'], unit_name = item_dict['unitName'] if 'unitName' in item_dict else None, category = item_dict['categories']['elements'][0]['name'], revenue = Revenue.objects.create()) 
+				Items.objects.create(name = item_dict['name'], cost = Cost.objects.create(item_cost = item_dict['cost']), price = item_dict['price'], price_type = item_dict['priceType'], unit_name = item_dict['unitName'] if 'unitName' in item_dict else None, category = item_dict['categories']['elements'][0]['name'], revenue = Revenue.objects.create(payment_type, quantity)) 
 
