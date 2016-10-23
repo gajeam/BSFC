723f2e63e7b4ee2ab5a06b149a20fd5de14afc81
Katie <katherinelatimer2013@u.northwestern.edu>
2016-10-22 17:21:36 -0700

    calculations for revenue, cogs, losses, profit by item category

--- BSFC/BSFC/public/category_calculations.py
+++ BSFC/BSFC/public/category_calculations.py
@@ -9,7 +9,6 @@ from BSFC.apps.revenue.constants import (
 def get_items_between_dates_by_category(category, start_date, end_date):
     return Item.objects.filter(category=category, created_at__gte=start_date, created_at__lte=end_date)
 
-
 def category_revenue(category, start_date, end_date):
     item_qs = get_items_between_dates_by_category(category, start_date, end_date)
     total_revenue = 0
@@ -17,7 +16,6 @@ def category_revenue(category, start_date, end_date):
         price = item.price #included in for loop to account for changing prices over time
         total_revenue += item.revenue.get_revenue_field(tender_choices['SOLD'])*price #minus refunds, discounts
     return total_revenue
-
 
 def category_cogs(category, start_date, end_date):
     item_qs = get_items_between_dates_by_category(category, start_date, end_date)
@@ -27,7 +25,6 @@ def category_cogs(category, start_date, end_date):
         total_cogs += item.revenue.get_revenue_field(tender_choices['SOLD'])*cost
     return total_cogs
 
-
 def category_losses(category, start_date, end_date, field_key):
     item_qs = get_items_between_dates_by_category(category, start_date, end_date)
     total_losses = 0
@@ -35,7 +32,6 @@ def category_losses(category, start_date, end_date, field_key):
         cost = item.cost.item_cost #included in for loop to account for changing costs over time
         total_losses += item.revenue.get_revenue_field(field_key)*cost
     return total_losses
-
 
 def category_profit(category, start_date, end_date):
     item_losses = 0
